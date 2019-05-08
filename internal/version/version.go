f6fb1f5b109cce0218d5d3fa4c50556dc1cf9c91
George Antoniadis <george@noodles.gr>
2019-05-08 01:00:38 +0100

    chore(version): add version package and global vars

---
+++ internal/version/version.go
@@ -0,0 +1,9 @@
+package version
+
+// Version information set a build time
+var (
+	Version     string // nolint: gochecknoglobals
+	Timestamp   string // nolint: gochecknoglobals
+	ServiceName string // nolint: gochecknoglobals
+	GitSHA      string // nolint: gochecknoglobals
+)

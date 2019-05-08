17e1f07974d28ce9142ecb883cc391aab0f6229b
George Antoniadis <george@noodles.gr>
2019-05-08 01:01:22 +0100

    feat(crawler): add working skeleton github crawler

---
+++ internal/crawler/crawler_github.go
@@ -0,0 +1,290 @@
+package crawler
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/go-github/v25/github"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	"github.com/kbariotis/go-discover/internal/queue"
+	"github.com/kbariotis/go-discover/internal/store"
+)
+
+type (
+	// Github crawler
+	Github struct {
+		followerPollInterval time.Duration
+
+		store  store.Store
+		client *github.Client
+
+		userOnboardingQueue queue.Queue
+		userFollowerQueue   queue.Queue
+		userQueue           queue.Queue
+		repositoryQueue     queue.Queue
+	}
+	// UserOnboardingTask represents a task in the userOnboarding queue
+	UserOnboardingTask struct {
+		Name string
+	}
+	// UserFollowerTask represents a task in the userFollower queue
+	UserFollowerTask struct {
+		Name string
+	}
+	// UserTask represents a task in the user queue
+	UserTask struct {
+		Name string
+	}
+	// RepositoryTask represents a task in the repository queue
+	RepositoryTask struct {
+		Name string
+	}
+)
+
+// NewGithub constructs a Github crawler
+func NewGithub(
+	followerPollInterval time.Duration,
+	store store.Store,
+	client *github.Client,
+	userOnboardingQueue queue.Queue,
+	userFollowerQueue queue.Queue,
+	userQueue queue.Queue,
+	repositoryQueue queue.Queue,
+) (*Github, error) {
+
+	sch := &Github{
+		store:                store,
+		client:               client,
+		followerPollInterval: followerPollInterval,
+		userOnboardingQueue:  userOnboardingQueue,
+		userFollowerQueue:    userFollowerQueue,
+		userQueue:            userQueue,
+		repositoryQueue:      repositoryQueue,
+	}
+
+	return sch, nil
+}
+
+// processOwnFollowers processes our own followers
+func (g *Github) processOwnFollowers() error {
+	logger := logrus.WithFields(logrus.Fields{
+		"logger": "crawler/Github.processOwnFollowers",
+	})
+
+	logger.Info("processing followers")
+
+	logger.Info("listing followers")
+
+	ctx := context.Background()
+
+	currentPage := 1
+	for currentPage != 0 {
+		opts := &github.ListOptions{
+			Page:    currentPage,
+			PerPage: 100,
+		}
+
+		followers, res, err := g.client.Users.ListFollowers(ctx, "", opts)
+		if err != nil {
+			return errors.Wrap(err, "could not retrieve followers")
+		}
+
+		logger.
+			WithFields(logrus.Fields{
+				"current_page":  currentPage,
+				"count":         len(followers),
+				"res.code":      res.StatusCode,
+				"res.next_page": res.NextPage,
+			}).
+			Debug("got followers")
+
+		for _, follower := range followers {
+			logger.
+				WithField("follower", follower.GetLogin()).
+				Debug("got follower, pushing to userFollowerQueue")
+
+			followerTask := &UserFollowerTask{
+				Name: follower.GetLogin(),
+			}
+
+			if err := g.userFollowerQueue.Push(followerTask); err != nil {
+				return errors.Wrap(err, "could add follower task to queue")
+			}
+		}
+
+		currentPage = res.NextPage
+	}
+
+	logger.Info("finished listing followers")
+
+	return nil
+}
+
+func (g *Github) handleUserOnboardingTask(task *UserOnboardingTask) error {
+	logger := logrus.WithFields(logrus.Fields{
+		"logger": "crawler/Github.handleUserOnboardingTask",
+		"task":   task,
+	})
+
+	logger.Info("handling UserOnboardingTask")
+
+	// TODO handle UserOnboardingTask
+
+	return nil
+}
+
+func (g *Github) handleUserFollowerTask(task *UserFollowerTask) error {
+	logger := logrus.WithFields(logrus.Fields{
+		"logger": "crawler/Github.handleUserFollowerTask",
+		"task":   task,
+	})
+
+	logger.Info("handling UserFollowerTask")
+
+	// TODO handle UserFollowerTask
+
+	return nil
+}
+
+func (g *Github) handleUserTask(task *UserTask) error {
+	logger := logrus.WithFields(logrus.Fields{
+		"logger": "crawler/handleUserTask.handleUserOnboardingTask",
+		"task":   task,
+	})
+
+	logger.Info("handling UserTask")
+
+	// TODO handle UserTask
+
+	return nil
+}
+
+func (g *Github) handleRepositoryTask(task *RepositoryTask) error {
+	logger := logrus.WithFields(logrus.Fields{
+		"logger": "crawler/Github.handleRepositoryTask",
+		"task":   task,
+	})
+
+	logger.Info("handling RepositoryTask")
+
+	// TODO handle RepositoryTask
+
+	return nil
+}
+
+// Start crawling
+func (g *Github) Start(ctx context.Context) error {
+	cctx, _ := context.WithCancel(ctx)
+	logger := logrus.WithFields(logrus.Fields{
+		"logger": "crawler/Github.Start",
+	})
+
+	userOnboardingTasks := make(chan *UserOnboardingTask, 10000)
+	userFollowerTasks := make(chan *UserFollowerTask, 10000)
+	userTasks := make(chan *UserTask, 10000)
+	repositoryTasks := make(chan *RepositoryTask, 10000)
+
+	// pop tasks from userOnboardingQueue and push them to a local channel
+	go func() {
+		logger.Info("starting to pop tasks from userOnboardingQueue")
+		for {
+			task, _ := g.userOnboardingQueue.Pop() // TODO handle error
+			if task == nil {
+				// time.Sleep(time.Second)
+				continue
+			}
+			if okTask, ok := task.(*UserOnboardingTask); ok {
+				userOnboardingTasks <- okTask
+			}
+		}
+	}()
+
+	// pop tasks from userFollowerQueue and push them to a local channel
+	go func() {
+		logger.Info("starting to pop tasks from userFollowerQueue")
+		for {
+			task, _ := g.userFollowerQueue.Pop() // TODO handle error
+			if task == nil {
+				// time.Sleep(time.Second)
+				continue
+			}
+			if okTask, ok := task.(*UserFollowerTask); ok {
+				userFollowerTasks <- okTask
+			}
+		}
+	}()
+
+	// pop tasks from userQueue and push them to a local channel
+	go func() {
+		logger.Info("starting to pop tasks from userQueue")
+		for {
+			task, _ := g.userQueue.Pop() // TODO handle error
+			if task == nil {
+				// time.Sleep(time.Second)
+				continue
+			}
+			if okTask, ok := task.(*UserTask); ok {
+				userTasks <- okTask
+			}
+		}
+	}()
+
+	// pop tasks from repositoryQueue and push them to a local channel
+	go func() {
+		logger.Info("starting to pop tasks from repositoryQueue")
+		for {
+			task, _ := g.repositoryQueue.Pop() // TODO handle error
+			if task == nil {
+				// time.Sleep(time.Second)
+				continue
+			}
+			if okTask, ok := task.(*RepositoryTask); ok {
+				repositoryTasks <- okTask
+			}
+		}
+	}()
+
+	// serially process events to reduce strain on the github api
+	// TODO introduce workers and proper request throttling for the api
+	followerPollTicker := time.NewTicker(g.followerPollInterval)
+	followerPollFirstPoll := time.After(0)
+	for {
+		select {
+		case <-cctx.Done():
+			return nil
+
+		case <-followerPollFirstPoll:
+			if err := g.processOwnFollowers(); err != nil {
+				return errors.Wrap(err, "first time processOwnFollowers failed")
+			}
+
+		case <-followerPollTicker.C:
+			if err := g.processOwnFollowers(); err != nil {
+				return errors.Wrap(err, "processOwnFollowers failed")
+			}
+
+		case task := <-userOnboardingTasks:
+			if err := g.handleUserOnboardingTask(task); err != nil {
+				return errors.Wrap(err, "failed to handle UserOnboardingTask")
+			}
+
+		case task := <-userFollowerTasks:
+			if err := g.handleUserFollowerTask(task); err != nil {
+				return errors.Wrap(err, "failed to handle UserFollowerTask")
+			}
+
+		case task := <-userTasks:
+			if err := g.handleUserTask(task); err != nil {
+				return errors.Wrap(err, "failed to handle UserTask")
+			}
+
+		case task := <-repositoryTasks:
+			if err := g.handleRepositoryTask(task); err != nil {
+				return errors.Wrap(err, "failed to handle RepositoryTask")
+			}
+
+		}
+	}
+}
